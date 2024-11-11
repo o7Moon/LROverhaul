08db252de3e8a887261834213e87d9df0b27591f
o7Moon <0167moon@gmail.com>
2024-11-11 11:57:03 -0600

    fix remount bone strength

--- src/Game/Physics/RiderConstants.cs
+++ src/Game/Physics/RiderConstants.cs
@@ -48,7 +48,7 @@ namespace linerider.Game
         }
         public const double EnduranceFactor = 0.0285;
         public const double RemountEnduranceMultiplier = 2.0;
-        public const double RemountStrengthMultiplier = 0.5;
+        public const double RemountStrengthMultiplier = 0.1;
         public const double StartingMomentum = 0.4;
         public static Vector2d Gravity = new Vector2d(0, 0.175); // Gravity
         public const int SledTL = 0;
